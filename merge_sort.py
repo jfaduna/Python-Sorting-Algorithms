a1f41d9b761f9bc5d7faefb16e27b1e9225d4bec
jfaduna <flomeraduna@gmail.com>
2023-11-04 11:16:21 +1100

    add: Added merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,49 @@
+from random import randint
+import time
+
+def merge_sort(array):
+    if len(array) < 2:
+        return array
+
+    midpoint = len(array) // 2
+
+    return merge(
+        left=merge_sort(array[:midpoint]),
+        right=merge_sort(array[midpoint:]))
+
+def merge(left, right):
+    if len(left) == 0:
+        return right
+
+    if len(right) == 0:
+        return left
+
+    result = []
+    index_left = index_right = 0
+
+    while len(result) < len(left) + len(right):
+        if left[index_left] <= right[index_right]:
+            result.append(left[index_left])
+            index_left += 1
+        else:
+            result.append(right[index_right])
+            index_right += 1
+
+        if index_right == len(right):
+            result += left[index_left:]
+            break
+
+        if index_left == len(left):
+            result += right[index_right:]
+            break
+
+    return result
+
+if __name__ == "__main__":
+    ARRAY_LENGTH = 10000
+    array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+    execution_start = time.time()
+    merge_sort(array)
+    execution_time = time.time() - execution_start
+
+    print(f"Execution Time: {execution_time:}")
